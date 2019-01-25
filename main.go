02e1baed95fd68b1cd383828050842dfbc652203
Hajime Hoshi <hajimehoshi@gmail.com>
2019-01-25 11:43:43 +0900

    Add new line

--- main.go
+++ main.go
@@ -71,6 +71,9 @@ func write(w io.Writer, r io.Reader) error {
 	if _, err := fmt.Fprintln(w, "\npackage " + *packageName); err != nil {
 		return err
 	}
+	if _, err := fmt.Fprintln(w, ""); err != nil {
+		return err
+	}
 	if *varIndex == "" {
 		if _, err := fmt.Fprintf(w, "var %s = []byte(%q)\n", *varName, string(bs)); err != nil {
 			return err
